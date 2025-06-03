933c37964b572d76084a158631b3c11b18bdb806
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-06-03 20:18:50 +0530

    fix: bootstrap events before server-startup

--- main.go
+++ main.go
@@ -7,8 +7,9 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/IAmRiteshKoushik/alfred/bootstrap"
 	"github.com/IAmRiteshKoushik/alfred/cmd"
-	"github.com/IAmRiteshKoushik/alfred/jobs"
+	"github.com/IAmRiteshKoushik/alfred/controller"
 	"github.com/IAmRiteshKoushik/alfred/middleware"
 	"github.com/IAmRiteshKoushik/alfred/pkg"
 	"github.com/gin-contrib/cors"
@@ -18,7 +19,7 @@ import (
 func main() {
 	failMsg := "[CRASH]: Could not start the app due to %s"
 
-	// Setup environment
+	// Setup environment variables
 	err := cmd.SetupEnv()
 	if err != nil {
 		log.Printf(failMsg, err)
@@ -32,7 +33,16 @@ func main() {
 	}
 	defer f.Close()
 	pkg.Log = pkg.NewLoggerService(cmd.AppConfig.Environment, f)
-	pkg.Log.SetupInfo("[ON]: Logging service has been activated.")
+	pkg.Log.SetupInfo("[ACTIVE]: Logging service is online.")
+
+	// Setup connection pooling for Postgres
+	pool, err := cmd.InitDB()
+	if err != nil {
+		pkg.Log.SetupFail("[CRASH]: Could not initialize database pool", err)
+		return
+	}
+	cmd.DBPool = pool
+	pkg.Log.SetupInfo("[ACTIVE]: Database pool has been created.")
 
 	// Setup valkey client
 	client, err := cmd.SetupValkey()
@@ -40,7 +50,13 @@ func main() {
 		return
 	}
 	pkg.Valkey = client
-	pkg.Log.SetupInfo("[ON]: Valkey service has been activated.")
+	pkg.Log.SetupInfo("[ACTIVE]: Valkey service is online.")
+
+	// Bootstrap Valkey data structures
+	check := bootstrap.BootstrapValkey()
+	if !check {
+		return
+	}
 
 	// Setup gin server
 	ginLogs, err := os.Create("gin.log")
@@ -65,8 +81,8 @@ func main() {
 		MaxAge:           12 * time.Hour,
 	}))
 
-	router.GET("/test", jobs.TestEndpointHandler)
-	router.POST("/webhook/:id", jobs.WebhookHandler)
+	router.GET("/test", controller.TestEndpointHandler)
+	router.POST("/webhook/:id", controller.WebhookHandler)
 
 	port := strconv.Itoa(cmd.AppConfig.ServerPort)
 	pkg.Log.SetupInfo("[ON]: Server configured and starting on PORT:" + port)
@@ -76,9 +92,9 @@ func main() {
 		pkg.Log.SetupFail("[FAIL]: Server failed to start due to:", err)
 		panic(routerErr)
 	}
-	pkg.Log.SetupInfo("[OFF]: Server deactivated.")
+	pkg.Log.SetupInfo("[DEACTIVE]: Server offline.")
 
 	cmd.CloseValkey(pkg.Valkey)
-	pkg.Log.SetupInfo("[OFF]: Valkey deactivated.")
-	pkg.Log.SetupInfo("[OFF]: Logging service deactivated.")
+	pkg.Log.SetupInfo("[DEACTIVE]: Valkey offline.")
+	pkg.Log.SetupInfo("[DEACTIVE]: Logging service offline.")
 }
