09ec62233f4f7ad6166a8220f8de07b121e5b35d
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-07-07 18:33:15 +0530

    feat: Handled all issue related events

--- controller/issue.go
+++ controller/issue.go
@@ -2,31 +2,58 @@ package controller
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"slices"
+	"strings"
 	"time"
 
 	"github.com/IAmRiteshKoushik/alfred/cmd"
 	"github.com/IAmRiteshKoushik/alfred/db"
 	"github.com/IAmRiteshKoushik/alfred/pkg"
 	"github.com/gin-gonic/gin"
+	"github.com/google/go-github/v62/github"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
-func handleIssueEvent(c *gin.Context, id uuid.UUID) {
-	// Only [ labelled, assigned, unassigned, closed, reopened ]
-	// issue-events are to be handled
-	switch event := c.GetString("event"); event {
+func handleIssueEvent(c *gin.Context, payload any) {
+	// Only [ labelled, assigned, unassigned, closed, reopened ] to be handled
+
+	issueEvent, ok := payload.(*github.IssueEvent)
+	if !ok {
+		pkg.Log.Error(c, "Failed to parse Issue-Event",
+			fmt.Errorf("Malformed event payload received in Issue-Event"),
+		)
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	username := *issueEvent.Issue.User.Login
+	issueUrl := *issueEvent.URL
+	event := *issueEvent.Event
+
+	switch event {
 	case "labelled":
-		issueLabelledAction(c, id)
-	case "assigned":
-		issueUserAction(c, id, event)
-	case "unassigned":
-		issueUserAction(c, id, event)
-	case "closed":
-		issueStateChangeAction(c, "closed")
-	case "reopened":
-		issueStateChangeAction(c, "reopened")
+		label := strings.ToLower(*issueEvent.Label.Name)
+		if label == "amsoc-accepted" {
+			title := *issueEvent.Issue.Title
+			repoUrl := *issueEvent.Repository.HTMLURL
+			issueAccepted(c, title, issueUrl, repoUrl)
+			return
+		}
+		if slices.Contains([]string{"easy", "medium", "hard"}, label) {
+			updateIssueDifficulty(c, issueUrl, label)
+			return
+		}
+		issueTagUpdate(c, issueUrl, label)
+		return
+	case "assigned", "unassigned":
+		issueUserAction(c, username, issueUrl, event)
+		return
+	case "closed", "reopened":
+		issueStateChangeAction(c, issueUrl, event)
+		return
 	default:
 		c.JSON(http.StatusOK, gin.H{
 			"message": "This issue event-type" + event + " is not handled.",
@@ -36,20 +63,10 @@ func handleIssueEvent(c *gin.Context, id uuid.UUID) {
 	}
 }
 
-func issueLabelledAction(c *gin.Context, id uuid.UUID) {
-	// TODO: Get issue details from context (parse the payload)
-	url := c.GetString("url")
-	title := c.GetString("title")
-	if url == "" || title == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "Issue URL or title not found",
-		})
-		pkg.Log.Warn(c, "Missing issue details - URL or title empty")
-		return
-	}
-
+func issueAccepted(c *gin.Context, title, repoUrl string, url string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+
 	tx, err := cmd.DBPool.Begin(ctx)
 	if err != nil {
 		pkg.Log.Error(c, "Failed to begin transaction", err)
@@ -58,10 +75,11 @@ func issueLabelledAction(c *gin.Context, id uuid.UUID) {
 		})
 		return
 	}
+	defer tx.Rollback(ctx)
 
 	q := db.New()
 	// Check if issue already exists and is open before attempting to create it
-	isOpen, err := q.CheckOpenIssueByUrlQuery(ctx, tx, url)
+	isOpen, err := q.CheckOpenIssueQuery(ctx, tx, url)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Failed to check issue status",
@@ -79,13 +97,12 @@ func issueLabelledAction(c *gin.Context, id uuid.UUID) {
 	}
 
 	params := db.AddNewIssueQueryParams{
-		ID:     uuid.New(),
-		Title:  title,
-		Repoid: id,
-		Url:    url,
+		ID:      uuid.New(),
+		Title:   title,
+		Repourl: repoUrl,
+		Url:     url,
 	}
-
-	_, err = q.AddNewIssueQuery(ctx, tx, params)
+	err = q.AddNewIssueQuery(ctx, tx, params)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Failed to add new issue",
@@ -98,33 +115,106 @@ func issueLabelledAction(c *gin.Context, id uuid.UUID) {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Failed to commit transaction",
 		})
-		pkg.Log.Error(c, "Failed to commit transaction", err)
+		pkg.Log.Fatal(c, "Failed to commit transaction", err)
 		return
 	}
 
-	pkg.Log.Info(c, "Successfully added new issue: "+url)
+	pkg.Log.Info(c, "Successfully added new issue: "+title)
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Issue added successfully",
 	})
-
 }
 
-func issueUserAction(c *gin.Context, id uuid.UUID, action string) {
-	// TODO: Fix url and username acquiring from context after parsing the request
-	url := c.GetString("url")
-	if url == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "Issue URL not found",
+func updateIssueDifficulty(c *gin.Context, issueUrl string, difficulty string) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	tx, err := cmd.DBPool.Begin(ctx)
+	if err != nil {
+		pkg.Log.Error(c, "Failed to begin transaction", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to check issue status",
 		})
 		return
 	}
-	username := c.GetString("username")
-	if username == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "Username not found",
+	defer tx.Rollback(ctx)
+
+	q := db.New()
+	params := db.UpdateIssueDifficultyQueryParams{
+		Difficulty: pgtype.Text{String: strings.ToTitle(difficulty), Valid: true},
+		Url:        issueUrl,
+	}
+	result, err := q.UpdateIssueDifficultyQuery(ctx, tx, params)
+	if err != nil || len(result) != 1 {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to update issue difficulty",
+		})
+		pkg.Log.Error(c, "Failed to update issue difficulty", err)
+		return
+	}
+
+	if err = tx.Commit(ctx); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to commit transaction",
+		})
+		pkg.Log.Fatal(c, "Failed to commit transaction", err)
+		return
+	}
+
+	pkg.Log.Info(c, "Successfully updated issue difficulty")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Issue added successfully",
+	})
+}
+
+func issueTagUpdate(c *gin.Context, issueUrl string, tag string) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	tx, err := cmd.DBPool.Begin(ctx)
+	if err != nil {
+		pkg.Log.Error(c, "Failed to begin transaction", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to check issue status",
 		})
 		return
 	}
+	defer tx.Rollback(ctx)
+
+	q := db.New()
+
+	params := db.AddIssueTagQueryParams{
+		ArrayAppend: []string{tag},
+		Url:         issueUrl,
+	}
+
+	tags, err := q.AddIssueTagQuery(ctx, tx, params)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to add issue tag",
+		})
+		pkg.Log.Error(c, "Failed to add issue tag", err)
+		return
+	}
+
+	if err = tx.Commit(ctx); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to commit transaction",
+		})
+		pkg.Log.Fatal(c, "Failed to commit transaction", err)
+		return
+	}
+
+	pkg.Log.Info(c, "Successfully updated issue difficulty")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Issue added successfully",
+		"tags":    tags,
+	})
+}
+
+func issueUserAction(c *gin.Context, username string, url string, action string) {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
@@ -142,20 +232,30 @@ func issueUserAction(c *gin.Context, id uuid.UUID, action string) {
 
 	switch action {
 	case "assigned":
-		params := db.CreateIssueClaimQueryParams{
-			Url:        url,
+		params := db.IssueAssignQueryParams{
+			IssueUrl:   url,
 			Ghusername: username,
 			ClaimedOn:  pgtype.Timestamp{Time: time.Now(), Valid: true},
 			ElapsedOn:  pgtype.Timestamp{Time: time.Now(), Valid: true},
 		}
-		err = q.CreateIssueClaimQuery(ctx, tx, params)
+		err = q.IssueAssignQuery(ctx, tx, params)
+		if err != nil {
+			pkg.Log.Error(c, "Failed to assign issue", err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
 
 	case "unassigned":
-		params := db.RemoveIssueClaimsQueryParams{
-			Url:        url,
+		params := db.IssueUnassignQueryParams{
+			IssueUrl:   url,
 			Ghusername: username,
 		}
-		_, err = q.RemoveIssueClaimsQuery(ctx, tx, params)
+		ok, err := q.IssueUnassignQuery(ctx, tx, params)
+		if err != nil || ok == "" {
+			pkg.Log.Error(c, "Failed to unassign issue", err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
 
 	default:
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -187,16 +287,7 @@ func issueUserAction(c *gin.Context, id uuid.UUID, action string) {
 	})
 }
 
-func issueStateChangeAction(c *gin.Context, state string) {
-	// TODO: Get issue URL from context after parsing the payload
-	url := c.GetString("url")
-	if url == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "Issue URL not found",
-		})
-		return
-	}
-
+func issueStateChangeAction(c *gin.Context, url string, state string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	tx, err := cmd.DBPool.Begin(ctx)
@@ -214,7 +305,7 @@ func issueStateChangeAction(c *gin.Context, state string) {
 	case "closed":
 		_, err = q.CloseIssueQuery(c, tx, url)
 	case "reopened":
-		_, err = q.ReopenIssueQuery(c, tx, url)
+		_, err = q.OpenIssueQuery(c, tx, url)
 	default:
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Invalid state change provided for issue",
