d1b8d251f838562c0c70f8cbb309faf523933545
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-07-07 06:35:28 +0530

    chore: PING event refactored

--- controller/ping.go
+++ controller/ping.go
@@ -1,43 +1,25 @@
 package controller
 
 import (
-	"context"
+	"fmt"
 	"net/http"
-	"time"
 
-	"github.com/IAmRiteshKoushik/alfred/cmd"
-	"github.com/IAmRiteshKoushik/alfred/db"
 	"github.com/IAmRiteshKoushik/alfred/pkg"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
+	"github.com/google/go-github/v62/github"
 )
 
-func handlePingEvent(c *gin.Context, id uuid.UUID) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	conn, err := cmd.DBPool.Acquire(ctx)
-	defer conn.Release()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Failed to connect to database",
-		})
-		pkg.Log.Error(c, "Database connection failed during ping event", err)
+func handlePingEvent(c *gin.Context, payload any) {
+
+	_, ok := payload.(github.PingEvent)
+	if !ok {
+		pkg.Log.Error(c, "Failed to parse PING event", fmt.Errorf("Malformed PING event payload received"))
+		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
 
-	queries := db.New()
-	repoName, err := queries.UpdateRepositoryOnboardedQuery(c, conn, id.String())
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Failed to mark repository as onboarded",
-		})
-		pkg.Log.Error(c, "Failed to update repository onboarding status", err)
-		return
-	}
-
-	pkg.Log.Info(c, "Repository "+repoName+" successfully onboarded")
+	pkg.Log.Info(c, "Repository successfully onboarded")
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Repository successfully onboarded",
+		"message": "PING processed successfully",
 	})
-
 }
