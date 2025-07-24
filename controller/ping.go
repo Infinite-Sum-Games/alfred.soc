dbcf49c3e40115c04b67c1131e69ab7a54cdc77b
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-07-24 14:33:32 +0530

    chore: Upgrading GitHub SDK to v74

--- controller/ping.go
+++ controller/ping.go
@@ -6,14 +6,16 @@ import (
 
 	"github.com/IAmRiteshKoushik/alfred/pkg"
 	"github.com/gin-gonic/gin"
-	"github.com/google/go-github/v62/github"
+	"github.com/google/go-github/v74/github"
 )
 
 func handlePingEvent(c *gin.Context, payload any) {
 
-	_, ok := payload.(github.PingEvent)
+	_, ok := payload.(*github.PingEvent)
 	if !ok {
-		pkg.Log.Error(c, "Failed to parse PING event", fmt.Errorf("Malformed PING event payload received"))
+		pkg.Log.Error(c, "Failed to parse PING event",
+			fmt.Errorf("Malformed PING event payload received"),
+		)
 		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
