e886c91b38acf91de5820c5ad5bf5788f97152d4
Ritesh Koushik <riteshkoushik39@gmail.com>
2026-01-03 05:20:10 +0530

    feat: Handle PR Opened, Merged and Closed differently

--- controller/pull_request.go
+++ controller/pull_request.go
@@ -20,7 +20,6 @@ type Solution struct {
 }
 
 func handlePullRequestEvent(c *gin.Context, payload any) {
-	// [opened, merged] are the only events being handled
 	prEvent, ok := payload.(*github.PullRequestEvent)
 	if !ok {
 		c.AbortWithStatus(http.StatusBadRequest)
@@ -30,14 +29,8 @@ func handlePullRequestEvent(c *gin.Context, payload any) {
 	prUrl := *prEvent.PullRequest.HTMLURL
 	repoUrl := *prEvent.Repo.HTMLURL
 	username := *prEvent.PullRequest.User.Login
-	isOpen := *prEvent.Action == "opened"
+	action := *prEvent.Action
 	isMerged := *prEvent.PullRequest.Merged
-
-	if !isOpen && !isMerged {
-		pkg.Log.Warn(c, "Will not handle events other than PR opening nad merging")
-		c.AbortWithStatus(http.StatusOK)
-		return
-	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
@@ -51,7 +44,8 @@ func handlePullRequestEvent(c *gin.Context, payload any) {
 	defer tx.Rollback(ctx)
 	q := db.New()
 
-	if isOpen {
+	switch action {
+	case "opened":
 		_, err := q.AddSolutionQuery(ctx, tx, db.AddSolutionQueryParams{
 			Url:        prUrl,
 			RepoUrl:    repoUrl,
@@ -82,55 +76,65 @@ func handlePullRequestEvent(c *gin.Context, payload any) {
 			c.AbortWithStatus(http.StatusInternalServerError)
 			return
 		}
+	case "closed":
+		if isMerged {
+			ok, err := q.CheckIfSolutionExist(ctx, tx, prUrl)
+			if err != nil {
+				pkg.Log.Error(c, "Could not check if solution exist", err)
+				c.AbortWithStatus(http.StatusInternalServerError)
+				return
+			}
+			if ok == 0 {
+				pkg.Log.Warn(c, "Solution does not exist")
+				c.AbortWithStatus(http.StatusBadRequest)
+				return
+			}
+
+			update, err := q.MergeSolutionQuery(ctx, tx, prUrl)
+			if err != nil {
+				pkg.Log.Error(c, "Could not update solution as merged", err)
+				c.AbortWithStatus(http.StatusInternalServerError)
+				return
+			}
+			if update == "" {
+				pkg.Log.Warn(c, "No solution with the given prUrl found")
+				c.AbortWithStatus(http.StatusBadRequest)
+				return
+			}
+
+			// Marshal data to send to redis
+			jsonData, err := json.Marshal(Solution{
+				Username: username,
+				Url:      prUrl,
+				Merged:   true,
+			})
+			if err != nil {
+				pkg.Log.Error(c, "Failed to marshal payload", err)
+				c.AbortWithStatus(http.StatusInternalServerError)
+				return
+			}
+
+			// Update redis for further processing of achievements
+			err = cmd.AddToStream(pkg.Valkey, pkg.SolutionMerge, string(jsonData))
+			if err != nil {
+				pkg.Log.Error(c, "Failed to insert into Redis", err)
+				c.AbortWithStatus(http.StatusInternalServerError)
+				return
+			}
+		} else {
+			_, err := q.DeleteSolutionQuery(ctx, tx, prUrl)
+			if err != nil {
+				pkg.Log.Error(c, "Could not delete solution", err)
+				c.AbortWithStatus(http.StatusInternalServerError)
+				return
+			}
+		}
+	default:
+		pkg.Log.Warn(c, "Will not handle events other than PR opening and merging")
+		c.AbortWithStatus(http.StatusOK)
+		return
 	}
 
-	if isMerged {
-		ok, err := q.CheckIfSolutionExist(ctx, tx, prUrl)
-		if err != nil {
-			pkg.Log.Error(c, "Could not check if solution exist", err)
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-		if ok == 0 {
-			pkg.Log.Warn(c, "Solution does not exist")
-			c.AbortWithStatus(http.StatusBadRequest)
-			return
-		}
-
-		update, err := q.MergeSolutionQuery(ctx, tx, prUrl)
-		if err != nil {
-			pkg.Log.Error(c, "Could not update solution as merged", err)
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-		if update == "" {
-			pkg.Log.Warn(c, "No solution with the given prUrl found")
-			c.AbortWithStatus(http.StatusBadRequest)
-			return
-		}
-
-		// Marshal data to send to redis
-		jsonData, err := json.Marshal(Solution{
-			Username: username,
-			Url:      prUrl,
-			Merged:   true,
-		})
-		if err != nil {
-			pkg.Log.Error(c, "Failed to marshal payload", err)
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-
-		// Update redis for further processing of achievements
-		err = cmd.AddToStream(pkg.Valkey, pkg.SolutionMerge, string(jsonData))
-		if err != nil {
-			pkg.Log.Error(c, "Failed to insert into Redis", err)
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-	}
-
-	// Common transaction manager closing statement for both isOpen and isMerged
 	if err := tx.Commit(ctx); err != nil {
 		pkg.Log.Fatal(c, "Could not commit transaction", err)
 		c.AbortWithStatus(http.StatusInternalServerError)
